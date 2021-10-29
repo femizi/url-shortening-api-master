d9595de376c7a09939092cc6803f2594a97a247f
Femi Efejuku <obito.femi@gmail.com>
2021-10-29 16:04:35 +0100

    created copy button

--- script.js
+++ script.js
@@ -2,34 +2,61 @@ const hamburger = document.querySelector('.hamburger')
 const hidden = document.querySelector('.cover')
 const form = document.querySelector('.form')
 const url =document.querySelector('#url')
-const copy = document.querySelector('.copy')
-const shortened = document.querySelector('#short')
+const copyBtn = document.querySelector('.copy')
+
 const answer = document.querySelector('.answer')
+mainBtn = document.querySelector('.main-btn')
+
+
+
+// add event listener to hamburger
 hamburger.addEventListener('click', openHidden)
 
 function openHidden(){
     hidden.classList.toggle("hidden")
+    hidden.classList.toggle("animate__fadeIn")
 }
 
+// Validate URl
+function ValidateUrl(link) 
+{
+ var urlFormat = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+	if(link.match(urlFormat)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 const results = document.createElement('div')
+// call api
 function result(url){
     fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)
     .then(res => res.json())
     .then(data =>{
-        
+        if (data.ok){
         var data= data
         results.innerHTML = `<div class="result">
         <div class="sent">${data.result.original_link}</div>
         <div class="shortened" id="short"><div>${data.result.short_link}</div>
-        <div class="copy-wrapper"></div><a href="#" class="copy" onClick= "copyLink()">Copy</a></div>
+        <div class="copy-wrapper"></div><a href="#" class="copy" onClick="copyLink('${data.result.short_link}')">Copy</a></div>
         </div>`
+        // create div 
+        
         answer.appendChild(results)
         
+        console.log(results.innerHTML.length)
         
+    }else {
+        alert('incorrect url')
+
+    }
+    mainBtn.innerHTML = "Shorten it!"
+    
  
     })
-    
-   
+  
+  
  }
 
 
@@ -37,27 +64,22 @@ form.addEventListener('submit', submit)
 
 function submit(e){
    e.preventDefault()
-    if(url.value.length === 0){
+    if(ValidateUrl(url.value)){
+        mainBtn.innerHTML = "Please wait..."
+        result(url.value)
+    }else{
+        
         url.style.outline = "solid 1px hsl(0, 87%, 67%)"
         alert('PLease input a link')
-    }else{
-        result(url.value)
-        
         
     }
 
 }
 
 
-copy.addEventListener('click', copyLink)
-
+// copy to clipboard
 function copyLink(data){
-    navigator.clipboard.writeText(data.result.short_link)
+    navigator.clipboard.writeText(data)
+    alert(`Your shortened Url "${data}" has been copied`)
 }
-
-
-
-
-
-
 
